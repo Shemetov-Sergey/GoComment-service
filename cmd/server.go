eb6585ff39bf0c03fc0b67e2b4da184982e53211
Shemetov Sergey <lucius-16-4-ever@yandex.ru>
2024-09-19 05:08:06 +0300

    Added interceptors for logging middleware

--- cmd/server.go
+++ cmd/server.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/Shemetov-Sergey/GoComment-service/pkg/config"
 	"github.com/Shemetov-Sergey/GoComment-service/pkg/db"
+	"github.com/Shemetov-Sergey/GoComment-service/pkg/middleware"
 	"github.com/Shemetov-Sergey/GoComment-service/pkg/pb"
 	"github.com/Shemetov-Sergey/GoComment-service/pkg/services"
 	"google.golang.org/grpc"
@@ -14,20 +15,33 @@ import (
 
 func main() {
 	c, err := config.LoadConfig()
+
 	if err != nil {
 		log.Fatalln("Failed at config", err)
 	}
+
 	h := db.Init(c.DBUrl)
+
 	lis, err := net.Listen("tcp", c.Port)
+
 	if err != nil {
 		log.Fatalln("Failed to listing:", err)
 	}
+
 	fmt.Println("Auth Svc on", c.Port)
+
 	s := services.Server{
 		H: h,
 	}
-	grpcServer := grpc.NewServer()
+
+	grpcServer := grpc.NewServer(
+		grpc.ChainUnaryInterceptor(
+			middleware.LoggingInterceptor,
+		),
+	)
+
 	pb.RegisterCommentServiceServer(grpcServer, &s)
+
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalln("Failed to serve:", err)
 	}
